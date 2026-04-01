dfcfb2dff8938ce319dba2fbb93bcd58d4f975d9
Sid <sidk4@example.com>
2026-04-01 12:24:26 +0530

    new

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/CPN_admin_panel/",
   plugins: [react()],
+  // server: {
+  //   allowedHosts: [
+  //     'cpnfoods.in',
+  //     'www.cpnfoods.in'
+  //   ]
+  // }
 })
